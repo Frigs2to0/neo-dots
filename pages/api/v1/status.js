8cb10fa50672f8e017dc570e15c0c61524006175
Guilherme <96925831+Frigs2to0@users.noreply.github.com>
2025-10-07 22:56:00 +0000

    add status db tests

--- pages/api/v1/status.js
+++ pages/api/v1/status.js
@@ -1,9 +1,27 @@
 import database from "infra/database.js"
 
 async function status(request, response) {
-    const result = await database.query('SELECT 1 + 1 as sum;')
-    console.log(result.rows)
-    response.status(200).json({chave: "FRIGS HAHA"})
+    const updatedAt = new Date().toISOString()
+
+    const dbVersionBody = await database.query("SHOW server_version;")
+    const dbVersion = await dbVersionBody.rows[0].server_version
+
+    const dbMaxConnectionsBody = await database.query("SHOW max_connections")
+    const dbMaxConnections = Number(dbMaxConnectionsBody.rows[0].max_connections)
+
+    const dbActiveConnectionsBody = await database.query("SELECT * FROM pg_stat_activity")
+    const dbActiveConnections = dbActiveConnectionsBody.rowCount
+       
+    response.status(200).json({
+        updated_at: updatedAt,
+        dependencies: {
+            database: {
+                db_version: dbVersion,
+                max_connections: dbMaxConnections,
+                db_active_connection: dbActiveConnections,
+            }
+        }
+    })
 }
 
 export default status
