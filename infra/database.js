31168bdcf93f0cc80293fc8a8ff29f7fb1867ce9
Guilherme <guiguigo4444@gmail.com>
2025-10-10 16:22:31 -0300

    change to neon db

--- infra/database.js
+++ infra/database.js
@@ -1,25 +1,14 @@
 import { Client } from "pg"
 
 async function query(queryObject) {
-     let client;
-
-  if (process.env.NODE_ENV === "production") {
-    client = new Client({
-      connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false 
-      }
-    });
-  } else {
-    client = new Client({
-      host: process.env.POSTGRES_HOST,
-      port: process.env.POSTGRES_PORT,
-      user: process.env.POSTGRES_USER,
-      database: process.env.POSTGRES_DB,
-      password: process.env.POSTGRES_PASSWORD,
-      ssl: false
-    });
-  }
+    const client = new Client({
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        user: process.env.POSTGRES_USER,
+        database: process.env.POSTGRES_DB,
+        password: process.env.POSTGRES_PASSWORD,
+        ssl: process.env.NODE_ENV === 'development' ? false : true,
+    })
     
     try {
         await client.connect()
